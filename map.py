de47dbf93f3b970a38bc35d4e68140a8925ff1ac
vseslav666 <yozhik30@yandex.ru>
2018-12-18 02:17:05 +0300

    much more lambdas

--- map.py
+++ map.py
@@ -1,26 +1,13 @@
 from functools import reduce
 
-
-def division_by_five(a):
-    a = a % 5
-    return a
-
-
 data = [1, 4, 5, 30, 99]
-print(list(map(division_by_five, data)))
-
-
-def int_to_str(a):
-    a = str(a)
-    return(a)
-
+print(list(map(lambda a: a % 5, data)))
 
 data = [3, 4, 90, -2]
-print(list(map(int_to_str, data)))
+print(list(map(lambda a: str(a), data)))
 
-
+data = ['some', 1, 'v', 40, '3a', str]
 print(list(filter(lambda x: type(x) != str, data)))
 
 data = ['some', 'other', 'value']
-
 print(len((reduce(lambda x, y: x + y, data))))
